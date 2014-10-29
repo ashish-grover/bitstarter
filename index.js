09435decee08e74a4b43deddd91b5e00f0f27619
ashish-grover <user.email>
2014-10-29 23:12:05 +0000

    Updated index.js to read from index.html

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  var data = fs.readFileSync('./index.html')
+  var dataString = data.toString()
+  response.send(dataString)
 })
 
 app.listen(app.get('port'), function() {
